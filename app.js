1c9b0f81f4b47ab8d5e2c14e3f4329d9801cf563
Shinta <65914727+shinjatta@users.noreply.github.com>
2021-04-27 01:16:23 +0200

    He fet el codi per aconseguir frases de yahoo

--- app.js
+++ app.js
@@ -1,52 +1,7 @@
-const puppeteer = require("puppeteer"); 
-
-(async ()=> {
-//Base configuració
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-//Cuidado esto lo borras despues shin
-const word="警察";
-//La web que vull buscar
-await page.goto('https://news.yahoo.co.jp/search?p=&ei=utf-8');
-//Buscar la paraula
-await page.type('input.sc-TOsTZ', word);
-await page.click("button.sc-cJSrbW");
-//Esperar a que es carreguin les diferents noticies
-await page.waitForTimeout(3000);
-const resultados = await page.evaluate(()=>{
-  //Els guardo els elements en dos arrays
-  //TEXTOS
-  const texts=document.querySelectorAll('.sc-ZOJMI bfJaxE');
-  //LINKS
-  const links= document.querySelectorAll('.newsFeed_item_link');
- //Creo el objeto final que voy a tener
- const resultadoFinal=[]
-  for (let i = 0; i < text.length; i++) { 
-    //Creo un objecto temporal
-    const resultado ={};
-    //Asocio a cada apartado que quiera que tenga la aplabra correspondiente
-    resultado.text=texts[i];
-    resultado.link=links[i];
-    //Lo pongo en el array final
-    resultadoFinal.push(resultado);
-  }
-});
-
-console.log(resultados);
-await browser.close();
-
-
-})();
-
-
-
-
-
-
-/* const express = require("express");
+const express = require("express");
 const cors = require("cors");
 const service_nhk = require("./services/NHK_service.js");
-
+const service_yahoo= require("./services/Yahoo_service.js");
 // create a new Express application instance
 const app = express();
 
@@ -60,12 +15,18 @@ app.listen(3000, () => {
 });
 
 // API call
-/* app.use(require('./apis/NHK_api')); 
+//app.use(require('./apis/NHK_api')); 
 
 app.get(`/frases/nhk/:word`, async (req, res) => {
   const palabra = req.params.word;
   const resultado = await service_nhk.NHK_service.getFrasesNHK(palabra);
   if (!resultado) res.sendStatus(400);
   res.status(200).json(resultado); 
-}); */
+});
 
+app.get(`/frases/yahoo/:word`, async (req, res) => {
+  const palabra = req.params.word;
+  const resultado = await service_yahoo.Yahoo_service.getFrasesYahoo(palabra);
+  if (!resultado) res.sendStatus(400);
+  res.status(200).json(resultado); 
+});
--- services/Yahoo_service.js
+++ services/Yahoo_service.js
@@ -1,40 +1,55 @@
+const { text } = require("body-parser");
 const puppeteer = require("puppeteer"); 
 class Yahoo_service {
 constructor (){
 
 }
 static async getFrasesYahoo(word){
-  //Base configuració
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  //La web que vull buscar
-  await page.goto('https://news.yahoo.co.jp/search?p=&ei=utf-8');
-  //Buscar la paraula
-  await page.type('input #sc-kgAjT bNteFw', word);
-  await page.click("button #sc-ksYbfQ LoaVT");
-  //Esperar a que es carreguin les diferents noticies
-  await page.waitForTimeout(3000);
-  const resultados = await page.evaluate(()=>{
-    //Els guardo els elements en dos arrays
-    //TEXTOS
-    const texts=document.querySelectorAll('.sc-ZOJMI');
-    //LINKS
-    const links= document.querySelectorAll('.newsFeed_item_link');
-   //Creo el objeto final que voy a tener
-   const resultadoFinal=[]
-    for (let i = 0; i < texts.length; i++) { 
-      //Creo un objecto temporal
-      const resultado ={};
-      //Asocio a cada apartado que quiera que tenga la aplabra correspondiente
-      resultado.text=texts[i];
-      resultado.link=links[i];
-      //Lo pongo en el array final
-      resultadoFinal.push(resultado);
-    }
-  });
-  
+//Base configuració
+const browser = await puppeteer.launch();
+try {
+const page = await browser.newPage();
+await page.setDefaultNavigationTimeout(0); 
+//La web que vull buscar
+await page.goto('https://news.yahoo.co.jp/search?p=&ei=utf-8');
+
+//Buscar la paraula
+await page.type('input.sc-TOsTZ', word);
+await page.click("button.sc-cJSrbW");
+//Esperar a que es carreguin les diferents noticies
+await page.waitForTimeout(3000);
+await page.screenshot({path: 'screenshot.png'});
+const resultados = await page.evaluate(()=>{
+  //Els guardo els elements en dos arrays
+  //TEXTOS
+  const texts=document.querySelectorAll('.sc-jYIdPM');
+  //LINKS
+  const links= document.querySelectorAll('.newsFeed_item_link');
+ //Creo el objeto final que voy a tener
+ const resultadoFinal=[]
+ //Faig que no en retorni més de 10
+ const numeroResultats=10;
+ if(texts.length<numeroResultats){
+   numeroResultats=texts.length;
+ }
+  for (let i = 0; i < numeroResultats; i++) { 
+    //Creo un objecto temporal
+    const resultado ={};
+    //Asocio a cada apartado que quiera que tenga la aplabra correspondiente y le digo inner text para tener le resultado de dentro del elemenyo
+    resultado.text=texts[i].innerText;
+    resultado.link=links[i].href;
+    //Lo pongo en el array final
+    resultadoFinal.push(resultado);
+  }
+  return resultadoFinal;
+});
+
+return resultados
+} catch (err) {
+  console.error(err.message);
+} finally {
   await browser.close();
-  return resultados;
+}
 };
 
 }
